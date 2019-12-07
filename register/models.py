064753b57c1f6cc5b46ad12954ac5a32d9053a91
Parsa Nooralinejad <parsanooralinejad@yahoo.com>
2019-12-07 16:19:42 +0330

    registration service completed

---
+++ register/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class employer(models.Model):
+    name = models.CharField(max_length=200)
+    opening = models.DateField();
+    pswd = models.CharField(max_length=40)
+    addr = models.CharField(max_length=2000)
+    phone = models.CharField(max_length=20)
+
+class employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    pswd = models.CharField(max_length=50)
+    gender = models.CharField(max_length=1) #'F' or 'M'
+    age = models.IntegerField()
+
+
---
+++ register/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from .models import employee
+from .models import employer
+# Create your views here.
+
+def register_an_employer(request):
+    emp_data = request.POST
+
+    if employer.objects.filter(name=emp_data['name']).exist():
+        return JsonResponse({'result':'employer already exists'},status=208)
+
+    employer_instance = employer.objects.create(name=emp_data['name']\
+                                                ,opening=emp_data['opening']\
+                                                ,pswd=emp_data['pswd']\
+                                                ,addr=emp_data['addr']\
+                                                ,phone=emp_data['phone'])
+    employer_instance.save()
+    return JsonResponse({'result':'OK'}, status=200)
+
+def register_an_employee(request):
+    emp_data = request.POST
+
+    if employee.objects.filter(username=emp_data['username']):
+        return JsonResponse({'result':'user already exists'}, status=208)
+
+    employee_instance = employee.objects.create(first_name=emp_data['firstname']\
+                                                ,surname=emp_data['surname']\
+                                                ,username=emp_data['username']\
+                                                ,pswd=emp_data['pswd']\
+                                                ,gender=emp_data['gender']\
+                                                ,age=emp_data['age'])
+
+    employee_instance.save()
+    return JsonResponse({'result': 'OK'}, status=200)
